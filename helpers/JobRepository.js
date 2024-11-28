e7a7cd4c60301a5f3806c5e960127e5c4f3e3a32
ouaala-ayoub <ouaalaayoub12@gmail.com>
2024-11-28 23:42:54 +0100

    update query logic

--- helpers/JobRepository.js
+++ helpers/JobRepository.js
@@ -20,18 +20,21 @@ class JobRepository extends BaseRepository {
     const page = parseInt(query.page) - 1 || 0;
     const skip = page * pageSize;
     const { page: _, ...searchQuery } = query;
+    const builder = {};
+    if (searchQuery.q) {
+      builder["title"] = {
+        $regex: searchQuery.q,
+        $options: "i",
+      };
+    }
+    if (searchQuery.cities) {
+      builder["location"] = { $in: searchQuery.cities.split(",") };
+    }
 
     return await this.model
       .find(
         //todo add another params and fix this check logic
-        searchQuery.q
-          ? {
-              title: {
-                $regex: searchQuery.q,
-                $options: "i",
-              },
-            }
-          : {}
+        builder
       )
       .select(select ?? "")
       .populate("hirerId")
